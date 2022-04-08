bf3acf7e10f2283a5f309c04d3e46f46179121c6
Adnan Sarkar <66450870+Aduvai1@users.noreply.github.com>
2022-04-08 14:53:07 +0600

    Correction!!

--- src/spring_2019/Question_3_part_b.java
+++ src/spring_2019/Question_3_part_b.java
@@ -3,6 +3,6 @@ package spring_2019;
 /*
 Is it a must for abstract classes to have abstract method/methods?
 
-Yes, abstract class must have at-least one abstract method.
+No, It's not mandatory for abstract class to have an abstract method.
 
 */
